80ac4cff0c74a20ec3e8f39085de1fefa83e37fc
aldrichsantiago-iswims <aldrichsantiago20@gmail.com>
2024-07-05 18:05:31 +0800

    added table to payroll

--- src/components/Payroll.tsx
+++ src/components/Payroll.tsx
@@ -1,7 +1,126 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from './ui/PageTitle'
+import TanstackTable from './ui/TanstackTable'
+import { createColumnHelper, getCoreRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
+import Modal from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+
+const columnHelper = createColumnHelper();
+  const columns = [
+    columnHelper.accessor('image', {
+      cell: url => <img src={url.getValue()} alt="" className="w-6 h-6 rounded-full bg-white" />
+    }),
+    columnHelper.accessor('employee_id', {
+      cell: (info) => info.getValue(),
+      header: () => <span>ID</span>,
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('full_name', {
+      cell: (info) => info.getValue(),
+      header: () => <span>Full Name</span>,
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('position_title', {
+      cell: (info) => info.getValue(),
+      header: () => <span>Position Title</span>,
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('employment_status', {
+      cell: (info) => info.getValue(),
+      header: () => <span>Employment Status</span>,
+      footer: (info) => info.column.id,
+    }),
+    columnHelper.accessor('department', {
+      cell: (info) => info.getValue(),
+      header: () => <span>Department</span>,
+      footer: (info) => info.column.id,
+    }),
+  ];
 
 export default function Payroll() {
+    const [printModal, setPrintModal] = useState(false)
+    const [data, setData] = useState([
+        {
+          "employee_id": "C101",
+          "full_name": "dragon1",
+          "employment_status": "retired",
+          "original_launch": "2010-12-08T15:43:00.000Z",
+          "original_launch_unix": 1291822980,
+          "missions": [
+              {
+                  "name": "COTS 1",
+                  "flight": 7
+              }
+          ],
+          "department": 1,
+          "position_title": "Dragon 1.0",
+          "image":"https://assets-global.website-files.com/5e51c674258ffe10d286d30a/5e535720d39923630d5487d7_peep-47.svg",
+          "details": "Reentered after three weeks in orbit",
+          "reuse_count": 0
+        },{
+          "employee_id": "C104",
+          "full_name": "dragon1",
+          "employment_status": "retired",
+          "original_launch": "2010-12-08T15:43:00.000Z",
+          "original_launch_unix": 1291822980,
+          "missions": [
+              {
+                  "name": "COTS 1",
+                  "flight": 7
+              }
+          ],
+          "department": 1,
+          "position_title": "Dragon 1.0",
+          "image":"https://assets-global.website-files.com/5e51c674258ffe10d286d30a/5e535720d39923630d5487d7_peep-47.svg",
+          "details": "Reentered after three weeks in orbit",
+          "reuse_count": 0
+        },{
+          "employee_id": "C103",
+          "full_name": "dragon1",
+          "employment_status": "retired",
+          "original_launch": "2010-12-08T15:43:00.000Z",
+          "original_launch_unix": 1291822980,
+          "missions": [
+              {
+                  "name": "COTS 1",
+                  "flight": 7
+              }
+          ],
+          "department": 1,
+          "position_title": "Dragon 1.0",
+          "image":"https://assets-global.website-files.com/5e51c674258ffe10d286d30a/5e535720d39923630d5487d7_peep-47.svg",
+          "details": "Reentered after three weeks in orbit",
+          "reuse_count": 0
+        },{
+          "employee_id": "C102",
+          "full_name": "dragon1",
+          "employment_status": "retired",
+          "original_launch": "2010-12-08T15:43:00.000Z",
+          "original_launch_unix": 1291822980,
+          "missions": [
+              {
+                  "name": "COTS 1",
+                  "flight": 7
+              }
+          ],
+          "department": 1,
+          "position_title": "Dragon 1.0",
+          "image":"https://assets-global.website-files.com/5e51c674258ffe10d286d30a/5e535720d39923630d5487d7_peep-47.svg",
+          "details": "Reentered after three weeks in orbit",
+          "reuse_count": 0
+        },
+      ])
+
+    const openModal = (stateModalFunction:  any) => stateModalFunction(true);
+    const closeModal = (stateModalFunction:  any) => stateModalFunction(false);
+
+    const employee_table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+      });
   return (
     <>
         <main id="content" className="flex-1 p-6 lg:px-8">
@@ -11,8 +130,28 @@ export default function Payroll() {
                         <PageTitle title='Payroll' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consequatur!'/>
                     </div>
                 </div>
+                <div className='flex justify-end'>
+                    <button className='text-white bg-slate-800 hover:bg-gray-700 py-2 px-8 m-2 rounded-md' onClick={()=>openModal(setPrintModal)}>Print</button>
+                    
+                </div>
+                <TanstackTable.Table table={employee_table}>
+                <TanstackTable.Caption>
+                  Payroll Processing
+                  <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+                    View all the payroll details of our employees.
+                  </p>
+                </TanstackTable.Caption>
+
+                <TanstackTable.THead isSortable />
+                <TanstackTable.TBody />
+              </TanstackTable.Table>
             </div>
+            <Modal open={printModal} onClose={()=>closeModal(setPrintModal)} center>
+                <h2>FILTERS</h2>
+
+            </Modal>
         </main>
+        
     </>
   )
 }
